32c4994b8b0772e2db0c9e83b7d513c3e8776d0e
Victor Chang <vichang@google.com>
2024-03-07 16:57:32 +0000

    Add test for SecureRandom.reseed()
    Bug: 328161596
    Bug: 328160610
    Test: atest CtsLibcoreTestCases:libcore.java.security.SecureRandomSpiTest
    Change-Id: I8df0819e2eabb0e10391d0276dba01e87fc778d0

--- luni/src/test/java/libcore/java/security/SecureRandomSpiTest.java
+++ luni/src/test/java/libcore/java/security/SecureRandomSpiTest.java
@@ -38,7 +38,7 @@ public class SecureRandomSpiTest {
 
     public static class MySecureRandomSpi extends SecureRandomSpi {
 
-        public final SecureRandomParameters myParams;
+        public SecureRandomParameters myParams;
 
         public MySecureRandomSpi() {
             myParams = null;
@@ -60,9 +60,13 @@ public class SecureRandomSpiTest {
             return new byte[0];
         }
 
+        @Override
         public void engineReseed(SecureRandomParameters params) {
+            myParams = params;
             super.engineReseed(params);
         }
+
+        @Override
         public SecureRandomParameters engineGetParameters() {
             return super.engineGetParameters();
         }
@@ -108,6 +112,20 @@ public class SecureRandomSpiTest {
     @Test
     public void testEngineReseed() {
         Assert.assertThrows(UnsupportedOperationException.class, () -> mSpi.engineReseed(null));
+    }
+
+    @Test
+    public void testReseed() throws NoSuchAlgorithmException {
+        SecureRandom secureRandom = SecureRandom.getInstance(MySecureRandomProvider.ALGORITHM,
+                MY_PARAMS);
+        assertEquals(MY_PARAMS.toString(), secureRandom.toString());
+        Assert.assertThrows(UnsupportedOperationException.class, () -> secureRandom.reseed());
+
+        assertEquals("null!", secureRandom.toString());
+        Assert.assertThrows(UnsupportedOperationException.class, ()
+                -> secureRandom.reseed(MY_PARAMS));
+        assertEquals(MY_PARAMS.toString(), secureRandom.toString());
+
     }
 
     @Test
